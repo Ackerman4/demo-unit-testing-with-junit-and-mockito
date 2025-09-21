2c5b33333b0d340a269f694373470b48e865fa85
AJ <moralesaj03@gmail.com>
2025-09-21 14:24:57 +0800

    Initial Commit

---
+++ src/main/java/unit_testing_demo/demo_unit_testing_with_junit_and_mockito/DemoUnitTestingWithJunitAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package unit_testing_demo.demo_unit_testing_with_junit_and_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUnitTestingWithJunitAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUnitTestingWithJunitAndMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unit_testing_demo/demo_unit_testing_with_junit_and_mockito/DemoUnitTestingWithJunitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package unit_testing_demo.demo_unit_testing_with_junit_and_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUnitTestingWithJunitAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
